d5fb277de50fb0ce8395aeba9b397ba90df0bfbe
underlinear <arduouschore@gmail.com>
2023-07-31 00:34:04 +0800

    Added little JS code(not functioning yet)

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
 const canvas = document.querySelector("#canvas");
 const colorPalette = document.querySelector("#color-palette").children;
-console.log(colorPalette);
+const tools = document.querySelector("#tools").children;
+
+function createCanvas(dimensions) {
+    for(let y = 0; y < dimensions; y++)
+    {
+        const row = document.createElement('div');
+        row.classList.add('canvasrow');
+        for(let x = 0; x < dimensions; x++)
+        {
+            const column = document.createElement('div');
+            column.classList.add('canvascolumn');
+            row.appendChild(column);
+        }
+    }
+}
