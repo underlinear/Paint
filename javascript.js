04933eafac4601d1b0bc8984b5fb5957f393811b
underlinear <arduouschore@gmail.com>
2023-08-01 02:55:05 +0800

    Created functions for setting canvas size and changing color selected background

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("#canvas");
-const colorPalette = document.querySelector("#color-palette").children;
+const colorPalette = Array.from(document.querySelector("#color-palette").children);
 const tools = document.querySelector("#tools").children;
+const colorSelected = document.querySelector("#color-selected img");
 
 function createCanvas(dimensions) {
     for(let y = 0; y < dimensions; y++)
@@ -13,5 +14,22 @@ function createCanvas(dimensions) {
             column.classList.add('canvascolumn');
             row.appendChild(column);
         }
+        canvas.appendChild(row);
     }
 }
+
+createCanvas(50);
+
+
+//TODO: The selected color will change the background color of the #color-selected img,
+//TODO: The selected color will change the background color of .canvascolumn hover effect.
+
+colorPalette.forEach((color) => {
+    color.addEventListener('click',(colorTarget) => {
+        const clickedColor = getComputedStyle(colorTarget.target);
+        const newColor = clickedColor.backgroundColor;
+        colorSelected.style.backgroundColor = newColor;
+        console.log(newColor);
+    })
+})
+
