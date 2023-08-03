d73ab402e681068bed0db788883916cf713d290b
underlinear <arduouschore@gmail.com>
2023-08-03 17:42:47 +0800

    Changed hover draw to click and drag draw

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector("#canvas");
 const colorPalette = Array.from(document.querySelector("#color-palette").children);
 const tools = document.querySelector("#tools").children;
 const colorSelected = document.querySelector("#color-selected img");
+
 function createCanvas(dimensions) {
     for(let y = 0; y < dimensions; y++)
     {
@@ -17,16 +18,59 @@ function createCanvas(dimensions) {
     }
 }
 
-createCanvas(32);
+function colorCanvas(pixel) {
+    pixel.target.style.backgroundColor = colorSelected.style.backgroundColor;
+    console.log(colorSelected.style.backgroundColor);
+}
+
+function handleMouseOver(event) {
+    colorCanvas(event);
+}
+
+function handleMouseDown(event) {
+    canvasPixel.forEach((pixel) => {
+    pixel.addEventListener('mouseover',handleMouseOver);
+    })
+}
+
+createCanvas(50);
 const canvasPixel = Array.from(document.querySelectorAll(".canvascolumn"));
 canvasPixel.forEach((pixel) => {
-    pixel.addEventListener('mouseover',(pixelHovered) => {
-        pixelHovered.target.style.backgroundColor = colorSelected.style.backgroundColor;
-        console.log(colorSelected.style.backgroundColor);
-    })
-})
+    pixel.addEventListener('mousedown', handleMouseDown)
+});
 
+window.addEventListener('mouseup',() => {
+    canvasPixel.forEach((pixel) => {
+        pixel.removeEventListener('mouseover',handleMouseOver)
+        })
+    }
+);
 
+// Named function for the mouseover event listener
+/*
+function handleMouseOver(event) {
+    colorCanvas(event);
+}
+
+// Named function for the mousedown event listener
+function handleMouseDown() {
+    canvasPixel.forEach((pixel) => {
+        pixel.addEventListener('mouseover', handleMouseOver);
+    });
+}
+
+// Attach the mousedown event listener to each .canvascolumn element
+canvasPixel.forEach((pixel) => {
+    pixel.addEventListener('mousedown', handleMouseDown);
+});
+
+// Remove the mouseover event listener from each .canvascolumn element on window mouseup
+window.addEventListener('mouseup', () => {
+    canvasPixel.forEach((pixel) => {
+        pixel.removeEventListener('mouseover', handleMouseOver);
+    });
+});
+*/
 //TODO: The selected color will change the background color of the #color-selected img,
 //TODO: The selected color will change the background color of .canvascolumn hover effect.
 
