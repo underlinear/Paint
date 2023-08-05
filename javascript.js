6bf0daeb7bf876af4fb217072dae36ce1a997596
underlinear <arduouschore@gmail.com>
2023-08-06 01:49:20 +0800

    Added togglable rainbow, grid and transparency pen

--- javascript.js
+++ javascript.js
@@ -2,14 +2,24 @@ const canvas = document.querySelector("#canvas");
 const colorPalette = Array.from(document.querySelector("#color-palette").children);
 const tools = document.querySelector("#tools").children;
 const colorSelected = document.querySelector("#color-selected img");
+const clearButton = document.querySelector("#clear");
+const gridButton = document.querySelector("#grid");
+const rainbowPenButton = document.querySelector("#rainbow");
+const darkenPenButton = document.querySelector("#darken");
+const penButton = document.querySelector("#pencil");
+const eraseButton = document.querySelector("#eraser");
+let gridActive = false;
+let rainbowPen = false;
+let normalPen = true;
+let darkenPen = false;
+let eraser = false;
+let fillColor = 'white';
 
 function createCanvas(dimensions) {
-    for(let y = 0; y < dimensions; y++)
-    {
+    for (let y = 0; y < dimensions; y++) {
         const row = document.createElement('div');
         row.classList.add('canvasrow');
-        for(let x = 0; x < dimensions; x++)
-        {
+        for (let x = 0; x < dimensions; x++) {
             const column = document.createElement('div');
             column.classList.add('canvascolumn');
             row.appendChild(column);
@@ -19,63 +29,120 @@ function createCanvas(dimensions) {
 }
 
 function colorCanvas(pixel) {
-    pixel.target.style.backgroundColor = colorSelected.style.backgroundColor;
-    console.log(colorSelected.style.backgroundColor);
+    let currentColor = window.getComputedStyle(colorSelected);
+    let targetPixel = window.getComputedStyle(pixel.target);
+    let redValue, greenValue, blueValue;
+    console.log(rainbowPen, darkenPen, normalPen, eraser);
+    if (rainbowPen === true) {
+        redValue = Math.floor(Math.random() * 256);
+        greenValue = Math.floor(Math.random() * 256);
+        blueValue = Math.floor(Math.random() * 256);
+        colorSelected.style.backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`;
+        pixel.target.style.backgroundColor = colorSelected.style.backgroundColor;
+    }
+    else if (darkenPen === true) {
+        let targetRgbValue = targetPixel.backgroundColor.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+        let currentRgbValue = currentColor.backgroundColor.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+        redValue = Math.floor((+targetRgbValue[1] + +currentRgbValue[1]) / 2);
+        greenValue = Math.floor((+targetRgbValue[2] + +currentRgbValue[2]) / 2);
+        blueValue = Math.floor((+targetRgbValue[3] + +currentRgbValue[3]) / 2);
+        /* FOR HEX VALUE ONLY
+        redValue = parseInt(pixel.target.style.backgroundColor.slice(1,3),16);
+        greenValue = parseInt(pixel.target.style.backgroundColor.slice(3,5),16);
+        blueValue = parseInt(pixel.target.style.backgroundColor.slice(5,7),16);
+        */
+        pixel.target.style.backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`;
+    }
+    else if (eraser === true) {
+        pixel.target.style.backgroundColor = fillColor;
+    }
+    else if (normalPen === true) {
+        pixel.target.style.backgroundColor = colorSelected.style.backgroundColor;
+    }
+    else {
+        alert("ERROR! How could this be?");
+    }
 }
 
 function handleMouseOver(event) {
     colorCanvas(event);
 }
 
-function handleMouseDown(event) {
+function handleMouseDown() {
     canvasPixel.forEach((pixel) => {
-    pixel.addEventListener('pointerover',handleMouseOver);
+        pixel.addEventListener('pointerover', handleMouseOver);
     })
 }
 
-createCanvas(50);
-const canvasPixel = Array.from(document.querySelectorAll(".canvascolumn"));
-canvasPixel.forEach((pixel) => {
-    pixel.addEventListener('pointerdown', handleMouseDown)
-});
-
-window.addEventListener('pointerup',() => {
-    canvasPixel.forEach((pixel) => {
-        pixel.removeEventListener('pointerover',handleMouseOver)
+function toggleGrid() {
+    gridActive = !gridActive;
+    if (gridActive === true) {
+        gridButton.classList.add('active-button');
+        canvasPixel.forEach((pixel) => {
+            pixel.style.boxShadow = "0px 0px 0px 1px #bdbdbd";
         })
     }
+    else {
+        gridButton.classList.remove('active-button');
+        canvasPixel.forEach((pixel) => {
+            pixel.style.boxShadow = "none";
+        })
+    }
+}
+
+function toggleTool(tool) {
+    normalPen = false;
+    rainbowPen = false;
+    darkenPen = false;
+    eraser = false;
+    if (tool === "normalPen") {
+        normalPen = true;
+    }
+    else if (tool === "rainbowPen") {
+        rainbowPen = true;
+    }
+    else if (tool === "darkenPen") { 
+        darkenPen = true 
+    }
+    else { 
+        eraser = true; 
+    }
+}
+
+createCanvas(16);
+
+const canvasPixel = Array.from(document.querySelectorAll(".canvascolumn"));
+canvasPixel.forEach((pixel) => {
+    pixel.addEventListener('pointerdown', handleMouseDown);
+    pixel.addEventListener('pointerdown', colorCanvas);
+});
+
+window.addEventListener('pointerup', () => {
+    canvasPixel.forEach((pixel) => {
+        pixel.removeEventListener('pointerover', handleMouseOver)
+    })
+}
 );
 
-// Named function for the mouseover event listener
-/*
-function handleMouseOver(event) {
-    colorCanvas(event);
-}
-
-// Named function for the mousedown event listener
-function handleMouseDown() {
+clearButton.addEventListener('click', () => {
     canvasPixel.forEach((pixel) => {
-        pixel.addEventListener('mouseover', handleMouseOver);
-    });
-}
-
-// Attach the mousedown event listener to each .canvascolumn element
-canvasPixel.forEach((pixel) => {
-    pixel.addEventListener('mousedown', handleMouseDown);
+        pixel.style.backgroundColor = "white"
+    })
 });
 
-// Remove the mouseover event listener from each .canvascolumn element on window mouseup
-window.addEventListener('mouseup', () => {
-    canvasPixel.forEach((pixel) => {
-        pixel.removeEventListener('mouseover', handleMouseOver);
-    });
-});
-*/
+gridButton.addEventListener('click', () => {toggleGrid});
+eraseButton.addEventListener('click', () => {toggleTool("eraser")});
+darkenPenButton.addEventListener('click', () => {toggleTool("darkenPen")});
+rainbowPenButton.addEventListener('click', () => {toggleTool("rainbowPen")});
+penButton.addEventListener('click', () => {toggleTool("normalPen")});
+
+
+
 //TODO: The selected color will change the background color of the #color-selected img,
 //TODO: The selected color will change the background color of .canvascolumn hover effect.
 
 colorPalette.forEach((color) => {
-    color.addEventListener('click',(colorTarget) => {
+    color.addEventListener('click', (colorTarget) => {
         const clickedColor = getComputedStyle(colorTarget.target);
         const newColor = clickedColor.backgroundColor;
         colorSelected.style.backgroundColor = newColor;
